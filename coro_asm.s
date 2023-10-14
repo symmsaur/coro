24a176b203484eb6360456e0f9c3357cbd054c08
Jonathan Karlsson <karlsjona@gmail.com>
2023-10-14 12:34:25 +0200

    Coroutines work!

--- coro_asm.s
+++ coro_asm.s
@@ -7,9 +7,9 @@
 coro_start:
     ;; To be able to restore later
     push rbp
+
     ;; store function pointer
     mov rax, [rdi+8]
-
     ;; Call funtion pointer passsing state
     ;; Coro.data is at coro+16
     mov rsi, [rdi+16]
@@ -18,31 +18,22 @@ coro_start:
     ;; Restore coroutine stack pointer
     mov rsp, [rdi]
     call rax
-    ;; We will end up here after yielding from the coroutine.
-    ;; Can still count on coro being in rdi.
-coro_return:
-    ;; Restore original stack pointer
-    mov rsp, [rdi+24]
-    ;; Restore registers
-    pop rbp
-    ret
 
     global coro_continue
 ;;; Continue coroutine
 ;;; Arguments:
 ;;;   rdi: Pointer to Coro struct
 coro_continue:
-    ;; store function pointer
-    mov rax, [rdi+8]
+    ;; To be able to restore later
+    push rbp
 
-    ;; Add stack setting up stuff here!
-
-    ;; Call funtion pointer passsing state
-    ;; Coro.data is at coro+16
-    mov rsi, [rdi+16]
-    call rax
-
-    mov rax, 1
+    ;; Store our stack pointer to enable restoring it
+    mov [rdi+24], rsp
+    ;; Restore coroutine stack pointer
+    mov rsp, [rdi]
+    ;; Restore registers from coroutine stack
+    pop rbp
+    ;; Return (out of coro_yield basically)
     ret
 
 ;;; Yield from coroutine
@@ -50,7 +41,12 @@ coro_continue:
 ;;;   rdi: Pointer to Coro struct
     global coro_yield
 coro_yield:
-    ;; Actually yield
-    ;; How do we get the coro struct pointer into here?
-    jmp coro_return
+    ;; Push registers to be able to restore
+    push rbp
+    ;; Store the stack pointer to be able to resume the coroutine
+    mov [rdi], rsp
+    ;; Restore original stack pointer
+    mov rsp, [rdi+24]
+    ;; Restore registers
+    pop rbp
     ret
--- test_coro.c
+++ test_coro.c
@@ -3,26 +3,26 @@
 #include "coro.h"
 
 void generator(void* ctx, void* output_raw) {
-    /* int* output = (int*)output_raw; */
-    /* int value = 0; */
-    /* while (1) { */
-    /*   value += 7; */
-    /*   *output = value; */
-    /*   coro_yield(ctx); */
-    /* } */
-    *(int*)output_raw = 7;
-    coro_yield(ctx);
+    printf("In generator\n");
+    int value = 0;
+    while (1) {
+      value += 7;
+      *(int*)output_raw = value;
+      printf("Generator yielded value %i\n", value);
+      coro_yield(ctx);
+    }
 }
 
 int main() {
     int value = 10;
     Coro* coro = coro_create(&generator, (void*)&value);
     coro_start(coro);
-    printf("%i\n", value);
-    /* coro_continue(coro); */
-    /* printf("%i\n", value); */
-    /* coro_continue(coro); */
-    /* printf("%i\n", value); */
+    printf("Got value from generator %i\n", value);
+    for (int i = 0; i < 10; i++)
+    {
+        coro_continue(coro);
+        printf("Got value from generator %i\n", value);
+    }
     coro_destroy(coro);
     return value;
 }
