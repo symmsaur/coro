0da807aabbf3f7ddf63cee897ed98027be8b349a
Jonathan Karlsson <karlsjona@gmail.com>
2023-10-15 16:09:11 +0200

    Set initial stack pointer to one past allocation
    The stack pointer points to the top of the stack. So anything that is
    allocated on the stack will end up on lower addresses than the stack
    pointer. Meaning we can put the stack pointer to the first address
    beyond the stack allocation.

--- coro.c
+++ coro.c
@@ -7,7 +7,7 @@ Coro* coro_create()
     Coro* coro = malloc(sizeof(Coro));
     // 4k stack per coroutine.
     coro->stack = malloc(4096);
-    coro->stack_pointer = coro->stack + 4095;
+    coro->stack_pointer = coro->stack + 4096;
     return coro;
 }
 
